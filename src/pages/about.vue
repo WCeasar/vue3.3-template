d0cb2e63f7d233210d2701d2800a65a4e849202d
WCeasar <285665110@qq.com>
2023-09-13 21:32:26 +0800

    feat: unocss 继承 icon(原理: unplugin-icons)

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -4,6 +4,16 @@ import { RouterLink } from 'vue-router/auto'
 
 <template>
   <div class="about-page">about</div>
+  <!-- A basic anchor icon from Phosphor icons -->
+  <div class="i-ph-anchor-simple-thin" />
+  <!-- An orange alarm from Material Design Icons -->
+  <div class="i-mdi-alarm text-orange-400" />
+  <!-- A large Vue logo -->
+  <div class="i-logos-vue text-3xl" />
+  <!-- Sun in light mode, Moon in dark mode, from Carbon -->
+  <button class="i-carbon-sun dark:i-carbon-moon" />
+  <button class="i-ph:airplane-tilt-duotone text-9xl" />
+  <!-- Twemoji of laugh, turns to tear on hovering -->
   <router-link to="/">index</router-link>
 </template>
 
